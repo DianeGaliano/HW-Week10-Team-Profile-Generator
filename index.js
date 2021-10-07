06f1e7bcdcf325e19aaa13b49c2b0485d60f7f08
Diane Galiano <dianegaliano09@gmail.com>
2021-10-06 22:29:25 -0500

    pushing tests and classes

--- index.js
+++ index.js
@@ -1,48 +1,65 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
+const Employee = require("./lib/Employee");
+const { Manager } = require("./lib/Manager");
+const { Engineer } = require("./lib/Engineer");
+const { Intern } = require("./lib/Intern");
+
+const employee = new Employee(`${response.employee}`, Manager);
+
+employee.getRole("Manager");
 
 
-//Constructor Function for 
-function Manager(name, employeeID, email, number ) {
-  this.name = name;
-  this.iD = iD;
-  this.email = email;
-  this.number = number;  
-}
 
-const teamManager = new Developer("teamManager", "JavaScript");
 
-Manager.prototype.printInfo = function () {
-    console.log(
-        `Name: ${this.name}\nID: ${this.employeeID}\nEmail: ${this.email}\nOffice Number: ${this.number}`
-    );
-    console.log("\n---------------\n");
-};
 
-function Engineer(name, iD, email, gitHub) {
-    this.name = name;
-    this.iD = iD;
-    this.email = email;
-    this.gitHub = gitHub; 
-}
 
-Engineer.prototype.printEngineer = function () {
-    console.log(
-        `Name: ${this.name}\nID: ${this.employeeID}\nEmail: ${this.email}\nGitHub: ${this.gitHub}`
-    );
-    console.log("\n---------------\n");
-};
 
-function Intern(name, iD, email, school) {
-    this.name = name;
-    this.iD = iD;
-    this.email = email;
-    this.school = school;
-}
 
-Intern.prototype.printIntern = function () {
-    console.log(
-        `Name: ${this.name}\nID: ${this.employeeID}\nEmail: ${this.email}\nSchool: ${this.school}`
-    );
-    console.log("\n---------------\n");
-};
+
+// const inquirer = require("inquirer");
+// const fs = require("fs");
+
+
+// //Constructor Function for 
+// function Manager(name, employeeID, email, number ) {
+//   this.name = name;
+//   this.iD = iD;
+//   this.email = email;
+//   this.number = number;  
+// }
+
+// const teamManager = new Developer("teamManager", "JavaScript");
+
+// Manager.prototype.printInfo = function () {
+//     console.log(
+//         `Name: ${this.name}\nID: ${this.employeeID}\nEmail: ${this.email}\nOffice Number: ${this.number}`
+//     );
+//     console.log("\n---------------\n");
+// };
+
+// function Engineer(name, iD, email, gitHub) {
+//     this.name = name;
+//     this.iD = iD;
+//     this.email = email;
+//     this.gitHub = gitHub; 
+// }
+
+// Engineer.prototype.printEngineer = function () {
+//     console.log(
+//         `Name: ${this.name}\nID: ${this.employeeID}\nEmail: ${this.email}\nGitHub: ${this.gitHub}`
+//     );
+//     console.log("\n---------------\n");
+// };
+
+// function Intern(name, iD, email, school) {
+//     this.name = name;
+//     this.iD = iD;
+//     this.email = email;
+//     this.school = school;
+// }
+
+// Intern.prototype.printIntern = function () {
+//     console.log(
+//         `Name: ${this.name}\nID: ${this.employeeID}\nEmail: ${this.email}\nSchool: ${this.school}`
+//     );
+//     console.log("\n---------------\n");
+// };
